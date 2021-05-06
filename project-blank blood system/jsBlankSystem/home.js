dfa9dce0d6829f84ce8d1745b665eff67110e5fd
DESKTOP-248CCMM\nguye <nguyenquocdung281099@gmail.com>
2021-05-06 15:21:45 +0700

    responsive

--- project-blank blood system/jsBlankSystem/home.js
+++ project-blank blood system/jsBlankSystem/home.js
@@ -1,34 +1,33 @@
-var $ = function(id){
-    return document.getElementById(id);
-}
+$(document).ready(function () {
+    $("#donate").hide();
+    $(".close").hide();
+    $(".mobile").hide();
+    $("#link-donate").click(function (e) { 
+        e.preventDefault();
+        $("#donate").show();
+    });
+    $("#time-close").click(function (e) { 
+        e.preventDefault();
+        $("#donate").hide();
+    });
+    $(".fa-bars").click(function (e) { 
+        e.preventDefault();
+        $(".mobile").slideDown();
+        $(".fa-bars").hide();
+        $(".close").show();
+    });
+    $(".close").click(function (e) { 
+        e.preventDefault();
+        $(".mobile").slideUp();
+    
+        $(".fa-bars").show();
+        $(".close").hide();
+    });
 
-
-//
-
-var validateForm= function(){
-    form.text[0] = $("fullname").value;
-    form.text[1] = $("Email").value;
-    form.text[2] = $("phone").value;
-    form.text[3] = $("your-message").value;
-    form.mail= form.text[1];
-    form.phone =form.text[2];
-    form.checkEmpty();
-    form.checkMail();
-    form.checkPhone();
-    if(form.isvalid){
-        $("contact-form").submit();
-    }
-}
-var handleButtonDonateLink = function(){
-    var className = "donate open";
-    $("donate").setAttribute("class",className);
-}
-var handleButtonDonateBtn = function(){
-    var className = "donate close";
-    $("donate").setAttribute("class",className);
-}
-window.onload= function(){
-    $("send").onclick = validateForm;
-    $("link-donate").onclick = handleButtonDonateLink;
-    $("time-close").onclick = handleButtonDonateBtn;
-}
+    $(".nav_item").click(function (e) { 
+        e.preventDefault();
+        $(".mobile").slideUp();
+        $(".fa-bars").show();
+        $(".close").hide();
+    });
+});
